3e5c340716f429aa94c422a3327a6de2dc4510ce
LukeMcCann <developer-lukemccann@outlook.com>
2021-03-07 13:36:15 +0000

    Add base HTML

--- Gruntfile.js
+++ Gruntfile.js
@@ -52,7 +52,7 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-node-sass');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-uglify');
-    // grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-watch');
 
     grunt.registerTask('default', 'watch');
---
+++ app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));
