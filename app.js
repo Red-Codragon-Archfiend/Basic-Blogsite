2e6ae96dd98e9c94a66c708fece32e82b584fe61
LukeMcCann <developer-lukemccann@outlook.com>
2021-03-07 14:05:58 +0000

    Add challenge 2 solution

--- app.js
+++ app.js
@@ -2,14 +2,26 @@
 
 require('dotenv').config();
 
+const LoremIpsum = require("lorem-ipsum").LoremIpsum;
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 
+const lorem = new LoremIpsum({
+    sentencesPerParagraph: {
+      max: 8,
+      min: 4
+    },
+    wordsPerSentence: {
+      max: 16,
+      min: 4
+    }
+});
+
 const port = process.env.PORT || 3000;
-const homStartingContent = '';
-const aboutContent = '';
-const contactContent = '';
+const homStartingContent = lorem.generateParagraphs(2);
+const aboutContent = lorem.generateParagraphs(2);
+const contactContent = lorem.generateParagraphs(2);
 
 const app = express();
 
@@ -19,7 +31,9 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.render('home');
+    res.render('home', {
+        homeParagraphContent: homStartingContent,
+    });
 });
 
 app.listen(port, () => {
