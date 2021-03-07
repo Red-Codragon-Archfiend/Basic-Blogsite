6ee31c1ea3c47b40b992a6958632001eb2f01c9b
LukeMcCann <developer-lukemccann@outlook.com>
2021-03-07 13:40:11 +0000

    Add port constant

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 'use strict';
 
+require('dotenv').config();
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
+
+const port = process.env.PORT || 3000;
 
 const app = express();
 
@@ -10,3 +14,8 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
+
+
+app.listen(port, () => {
+    console.log(`Listening on port: ${port}`)
+});
